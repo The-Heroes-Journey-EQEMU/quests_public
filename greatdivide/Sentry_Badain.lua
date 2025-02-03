6f8381e9ab76490e1ae46f13705d66d6602cff74
piattnd <44480957+piattnd@users.noreply.github.com>
2025-02-03 16:11:35 -0700

    Ring 10 war overhaul (#1)

--- greatdivide/Sentry_Badain.lua
+++ greatdivide/Sentry_Badain.lua
@@ -1,6 +1,9 @@
 local war_started = false
 
 function event_spawn(e)
+	if (tostring(eq.get_zone_instance_version()) != eq.get_rule("Custom:StaticInstanceVersion")) then -- Only spawn Badain in non-respawning dz
+		e.self:Depop();
+	end
 	war_started = false
 end
 
@@ -14,7 +17,6 @@ function event_trade(e)
 	local item_lib = require("items");
 
 	if not war_started and item_lib.check_turn_in(e.trade, {item1 = 30369, item2= 1567}) then -- 9th ring and declaration of war
-		e.other:AddEXP(20000);
 		e.other:SummonItem(30369);
 		e.self:Say(string.format("I'll be right with you, %s.",e.other:GetCleanName()));
 		e.self:AddItem(30645, 0, true);
@@ -30,7 +32,7 @@ end
 function event_timer(e)
 	if e.timer == "smallPause" then
 		eq.stop_timer("smallPause")
-		eq.zone_emote(MT.DimGray, 'The sound of a mighty horn echoes through the area. All local inhabitants begin to scurry to take cover... then they realize nothing is coming for them. The tenth ring war is coming... but not yet! Check Discord for updates.');
-		eq.load_encounter("ring_war");
+		eq.zone_emote(1, 'The sound of a mighty horn echoes through the area. All local inhabitants scurry to take cover.');
+		eq.load_encounter("RingTen");
 	end
 end
