470a7939d34103e9ed7a9b44c13e9a71d8a7209b
Trust <fryguy503@gmail.com>
2025-02-06 23:24:27 -0500

    spell adj

--- akheva/179141.lua
+++ akheva/179141.lua
@@ -1,6 +1,6 @@
 function event_combat(e)
 	if e.joined then
-		e.self:CastSpell(2817, 0); -- Spell: Thought Vortex
+        e.self:CastedSpellFinished(eq.ChooseRandom(2817,2816), e.self:GetHateTop()); -- Spell: Thought Vortex, Storm Tremor
 		eq.depop_with_timer();
 	end
 end
