8aec010b6bc2f150d21e4af49db207113658c82a
Zimp <zimp@zenryo.xyz>
2025-02-08 02:42:28 +0000

    [ssratemple] Fix double/respawning quest triggers for Cursed/Rhag.

--- ssratemple/encounters/Cursed_Cycle.lua
+++ ssratemple/encounters/Cursed_Cycle.lua
@@ -101,16 +101,16 @@ function reset(e, new_state)
 	eq.depop_all(cursed_id);
 
 	set_state(new_state);
-	check_state(e);
+	check_state(e, true);
 end
 
-function check_state(e)
+function check_state(e, check_start)
 	local state = get_state();
 	if state == -1 then
 		return
 	end
 
-	if state == STATE_START then
+	if check_start and state == STATE_START then
 		eq.unique_spawn(controller_id, 0, 0, 0, 0, 0, 0);
 		local entity_list = eq.get_entity_list();
 		for i,id in ipairs(serpent_triggers) do
@@ -202,5 +202,5 @@ function event_encounter_load(e)
 
 	eq.register_player_event(Event.say, GMControl);
 
-	check_state(e);
+	check_state(e, false);
 end
